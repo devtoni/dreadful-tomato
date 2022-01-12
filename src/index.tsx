97d6f17a7302805520f62197017327ba8d5ae774
Toni Ruiz <toni.ruizdev@gmail.com>
2022-01-12 12:46:13 +0100

    Use msw in dev to bypass cors problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { worker } from './infrastructure/msw/browser';
 
 import App from './App';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.render(
   <React.StrictMode>
